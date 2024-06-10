1062777b298aebf366dde77d4dd65db64f694e53
Jhiro F. <jhiraf07@gmail.com>
2024-06-10 21:41:08 +0700

    [CREATE] new project leaderboard

---
+++ src/main/java/com/example/jhiro/game_leaderboard/enigma/EnigmaApplication.java
@@ -0,0 +1,13 @@
+package com.example.jhiro.game_leaderboard.enigma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnigmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnigmaApplication.class, args);
+	}
+
+}
